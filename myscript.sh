2fdcc65917d1f64f98bffe338fc0edd820d52d41
Tajveez Rehman <tajveez786@gmail.com>
2021-04-05 23:44:50 +0500

    Create myscript.sh

---
+++ myscript.sh
@@ -0,0 +1,90 @@
+#! /usr/bin/bash
+
+# ECHO Command
+# echo Hello World!
+
+# VARIABLES
+# Uppercase by convention
+# NAME="Tajveez"
+# echo "My name is $NAME"
+# echo "My name is ${NAME}"
+
+# INPUT
+# read -p "Whats your business: " business
+# echo "$business is a nice business"
+# business="designer"
+
+# # SIMPLE IF Statement
+# if [ "$business" == "developer" ]; then
+#     echo "Welcome to dev world mate!"
+# else
+#     echo "move on mate, we don't need $business here"
+# fi
+
+# COMPARISON
+# NUM1=31
+# NUM2=5
+# if [ "$NUM1" -gt "$NUM2" ]; then
+#     echo "$NUM1 is greater than $NUM2"
+# else
+#     echo "$NUM1 is less than $NUM2"
+# fi
+
+########
+# val1 -eq val2 Returns true if the values are equal
+# val1 -ne val2 Returns true if the values are not equal
+# val1 -gt val2 Returns true if val1 is greater than val2
+# val1 -ge val2 Returns true if val1 is greater than or equal to val2
+# val1 -lt val2 Returns true if val1 is less than val2
+# val1 -le val2 Returns true if val1 is less than or equal to val2
+########
+
+# FILE CONDITIONS
+# file="test.txt"
+# if [ -e "$file" ]; then
+#     echo "$file, is a file"
+# else
+#     echo "$file is not a file."
+# fi
+
+########
+# -d file   True if the file is a directory
+# -e file   True if the file exists (note that this is not particularly portable, thus -f is generally used)
+# -f file   True if the provided string is a file
+# -g file   True if the group id is set on a file
+# -r file   True if the file is readable
+# -s file   True if the file has a non-zero size
+# -u    True if the user id is set on a file
+# -w    True if the file is writable
+# -x    True if the file is an executable
+########
+
+# CASE STATEMENT
+# read -p "Do you want to continue? Y/N" answer
+# case "$answer" in
+# [yY] | [yY][eE][sS])
+#     echo "Proceeding... :)"
+#     ;;
+# [nN] | [nN][oO])
+#     echo "Okay :/"
+#     ;;
+# *)
+#     echo "please answer in yes/no"
+#     ;;
+# esac
+
+# SIMPLE FOR LOOP
+# names="Ali Ahmed Adnan Adil"
+# for name in $names; do
+#     echo "Hello $name"
+# done
+
+# FOR LOOP TO RENAME FILE
+# FILES=$(ls *.txt)
+# NEW="new"
+# for FILE in $FILES; do
+#     echo "renaming $FILE to new-$FILE"
+#     mv $FILE $NEW-$FILE
+# done
+
+# WHILE LOOP - Read file line by line
